ca2856b1b6b1d6f0eda416d5eddc97fe76ebde35
AndrejZelonka <andrej.zelonka@gmail.com>
2026-05-29 22:30:11 +0200

    feat: present date-picker

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { DateRangePickerComponent } from './components/date-picker/date-picker';
 import { PRESETS } from './presets/presets';
+import { DateRange } from './components/date-picker/types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   imports: [DateRangePickerComponent],
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly presets = PRESETS;
+
+  protected onRangeChange(range: DateRange): void {
+    console.log('Date range: ', range);
+  }
 }
